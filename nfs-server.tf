7f6797ba839c89153afaaf65efbe3453e528092c
mborne <mborne@users.noreply.github.com>
2023-04-18 08:25:32 +0200

    renommage nfs-instance en nfs-server

---
+++ nfs-server.tf
@@ -0,0 +1,19 @@
+# Création d'un stockage NFS ayant vocation à être utilisé pour fournir un stockage RWX
+# avec https://github.com/kubernetes-sigs/nfs-subdir-external-provisioner
+#
+# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/filestore_instance
+resource "google_filestore_instance" "nfs_server" {
+  name     = "nfs-instance"
+  location = var.zone_name
+  tier     = "STANDARD"
+
+  file_shares {
+    capacity_gb = 1024
+    name        = "gke_share"
+  }
+
+  networks {
+    network = "default"
+    modes   = ["MODE_IPV4"]
+  }
+}
